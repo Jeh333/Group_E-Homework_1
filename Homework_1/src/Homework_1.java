6403c5d60993848d34412e665a7b857eb09ebbd2
bsimpson0 <91200121+bsimpson0@users.noreply.github.com>
2024-01-31 16:51:19 -0600

    Updated Homework_1.java by adding my info

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -4,5 +4,6 @@ public class Homework_1 {
 		public static void main(String[] args) {
 			System.out.println("Name: Jonathan Hatfield \nGithub ID: 116123209 \n");
 			System.out.println("Name: Brian Bluhm \nGithub ID: 116319535 \n");
+			System.out.println("Name: Blake Simpson \nGithub ID: 91200121 \n");
 		}
 }
