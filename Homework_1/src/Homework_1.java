0fb4fe10f93759fce6024f19dc935867361861db
ethanalexander <ela6m6@umsystem.edu>
2024-02-01 23:50:46 -0600

    changed name and id

--- Homework_1/src/Homework_1.java
+++ Homework_1/src/Homework_1.java
@@ -5,5 +5,6 @@ public class Homework_1 {
 			System.out.println("Name: Jonathan Hatfield \nGithub ID: 116123209 \n");
 			System.out.println("Name: Brian Bluhm \nGithub ID: 116319535 \n");
 			System.out.println("Name: Blake Simpson \nGithub ID: 91200121 \n");
+			System.out.println("Name: Ethan Alexander \nGithub ID: 157988000 \n");
 		}
 }
